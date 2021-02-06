cb8ace8eb78c28588bce8a34bbc9d950313aa48d
luanacesar <luana_cesar@hotmail.com>
2021-02-06 17:47:04 -0500

    setting for github

---
+++ index.js
@@ -0,0 +1,29 @@
+//python
+document.getElementById("py").onclick = function () {
+	location.href = "https://github.com/luanacesar/Python";
+};
+//SQL
+document.getElementById("sql").onclick = function () {
+	location.href = "https://github.com/luanacesar/SQL";
+};
+// REACT
+document.getElementById("react").onclick = function () {
+	location.href = "https://github.com/luanacesar/React";
+};
+//C#
+document.getElementById("csharp").onclick = function () {
+	location.href = "https://github.com/luanacesar/C_SHARP";
+};
+//node
+document.getElementById("node").onclick = function () {
+	location.href = "https://github.com/luanacesar/Node.js";
+};
+//working in group with git
+document.getElementById("git").onclick = function () {
+	location.href = "https://github.com/luanacesar/OneClickCleaner";
+};
+
+
+
+
+
